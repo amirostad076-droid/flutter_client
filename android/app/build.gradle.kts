a7e88b487be13bcd28e073a8b7682bdd633f86b4
M0n7y5 <17201053+M0n7y5@users.noreply.github.com>
2026-05-12 00:06:28 +0200

    fix(android): enable notification desugaring

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -23,6 +23,7 @@ android {
     flavorDimensions += listOf("environment", "push")
 
     compileOptions {
+        isCoreLibraryDesugaringEnabled = true
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
     }
@@ -100,4 +101,8 @@ android {
 
 flutter {
     source = "../.."
+}
+
+dependencies {
+    coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:2.1.4")
 }
