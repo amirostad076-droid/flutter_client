e298cad703287a8454f4191ce4dcd7afdf5ee086
Elias <elias@deuss.dev>
2026-05-19 20:30:41 +0200

    fix(android): setup configureImagePicker

--- lib/main.dart
+++ lib/main.dart
@@ -5,11 +5,24 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:fluxer_app/app.dart';
 import 'package:fluxer_app/core/providers/app_startup_provider.dart';
+import 'package:image_picker_android/image_picker_android.dart';
+import 'package:image_picker_platform_interface/image_picker_platform_interface.dart';
 import 'package:media_kit/media_kit.dart';
 import 'package:window_manager/window_manager.dart';
 
+void _configureImagePicker() {
+  if (kIsWeb || !Platform.isAndroid) {
+    return;
+  }
+  final ImagePickerPlatform implementation = ImagePickerPlatform.instance;
+  if (implementation is ImagePickerAndroid) {
+    implementation.useAndroidPhotoPicker = true;
+  }
+}
+
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  _configureImagePicker();
   MediaKit.ensureInitialized();
 
   if (!kIsWeb && (Platform.isLinux || Platform.isMacOS || Platform.isWindows)) {
