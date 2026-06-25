b7f2737d54d1d2a2900ebcddcdbc511c9d91864e
Elias <elias@deuss.dev>
2026-06-25 18:39:51 +0200

    chore(talker): change non debug mode settings

--- lib/core/talker.dart
+++ lib/core/talker.dart
@@ -1,8 +1,16 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:talker/talker.dart';
 import 'package:talker_flutter/talker_flutter.dart' as talker_ui;
 
-final talker = Talker();
+final talker = Talker(
+  settings: TalkerSettings(
+    enabled: true,
+    useHistory: true,
+    useConsoleLogs: kDebugMode,
+    maxHistoryItems: kDebugMode ? 1000 : 500,
+  ),
+);
 
 /// Pushes the Talker log viewer
 Future<void> pushTalkerLogScreen(
