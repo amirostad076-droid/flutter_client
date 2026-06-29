db32a044e2688314c2d94e153a6de282e04934b7
Jiralite <33201955+Jiralite@users.noreply.github.com>
2026-06-29 14:17:24 +0100

    chore(gen): flutter gen-l10n

--- lib/l10n/generated/fluxer_localizations_ga.dart
+++ lib/l10n/generated/fluxer_localizations_ga.dart
@@ -1358,7 +1358,7 @@ class FluxerLocalizationsGa extends FluxerLocalizations {
 
   @override
   String get avatarDescription =>
-      'JPEG, PNG, GIF, WebP. Max 10MB. Recommended: 512×512px';
+      'PNG, JPEG, WebP, GIF. Max 10MB. Recommended: 512×512px';
 
   @override
   String get bannerLabel => 'Banner';
@@ -1371,7 +1371,7 @@ class FluxerLocalizationsGa extends FluxerLocalizations {
 
   @override
   String get bannerDescription =>
-      'JPEG, PNG, GIF, WebP. Max 10MB. Minimum: 680×240px (17:6)';
+      'PNG, JPEG, WebP, GIF. Max 10MB. Minimum: 680×240px (17:6)';
 
   @override
   String get accentColorLabel => 'Accent Color';
