dcdef2b8f8a9d3802f014c91031c979c3d4df372
Elias <elias@deuss.dev>
2026-07-11 21:32:36 +0200

    fix(settings): move native name to subtitle

--- lib/shared/utils/app_locale_display.dart
+++ lib/shared/utils/app_locale_display.dart
@@ -15,7 +15,7 @@ class AppLocaleDisplayInfo {
   final String languageCode;
   final String? countryCode;
 
-  String get label => '$name ($nativeName)';
+  String get label => name;
 
   String get searchText => '$name $nativeName $languageCode';
 }
@@ -258,9 +258,9 @@ AppLocaleDisplayInfo appLocaleDisplayInfo(sdk.Locale locale) {
 List<sdk.Locale> sortedAppSdkLocales() {
   final List<sdk.Locale> locales =
       List<sdk.Locale>.from(sdk.Locale.$valuesDefined)..sort(
-        (sdk.Locale a, sdk.Locale b) => appLocaleDisplayInfo(a).label
+        (sdk.Locale a, sdk.Locale b) => appLocaleDisplayInfo(a).name
             .toLowerCase()
-            .compareTo(appLocaleDisplayInfo(b).label.toLowerCase()),
+            .compareTo(appLocaleDisplayInfo(b).name.toLowerCase()),
       );
   return locales;
 }
