20ca390112ca2d281e6f6fcb6e5aa984716371ef
Hampus Kraft <hampus@fluxer.com>
2026-08-29 20:55:52 +0200

    fix(push): skip background local notification for clear payloads

--- lib/core/push/fcm/fcm_entrypoint.fcm.dart
+++ lib/core/push/fcm/fcm_entrypoint.fcm.dart
@@ -7,6 +7,7 @@ import 'package:fluxer_app/core/push/fcm/fcm_android_system_notification_cancel_
 import 'package:fluxer_app/core/push/fcm/fcm_background_handler_policy.dart';
 import 'package:fluxer_app/core/push/fcm/fcm_pending_notification_tap.dart';
 import 'package:fluxer_app/core/push/fcm/fcm_tap_payload_cache.dart';
+import 'package:fluxer_app/core/push/push_notification_payload.dart';
 import 'package:fluxer_fcm/fcm_background_handler.dart';
 import 'package:fluxer_fcm/fcm_push_message.dart';
 import 'package:fluxer_fcm/fluxer_fcm_bootstrap.dart';
@@ -30,6 +31,9 @@ void _configureFcmBootstrap() {
 }
 
 bool _shouldDisplayBackgroundLocal(FcmPushMessage message) {
+  if (isNotificationClearPayload(message.payload)) {
+    return false;
+  }
   return shouldSaveFcmTapPayloadCache(message.payload);
 }
 
